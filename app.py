75128533800cd3501d6f06a25f6454a1ad4893fe
RadekRo <r.roclawski@ankara.pl>
2023-07-04 15:27:04 +0200

    Added bcrypt, session and data return with user

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
-from flask import Flask, render_template, url_for, request, jsonify
+from flask import Flask, render_template, url_for, request, jsonify, session
 import mimetypes
+import bcrypt
 
 mimetypes.add_type('application/javascript', '.js')
 app = Flask(__name__)
+app.secret_key = bcrypt.gensalt()
 
 @app.route("/")
 def index():
@@ -10,13 +12,15 @@ def index():
 
 @app.route("/chat")
 def chat():
+    
+    session['username'] = 'Radek'
     return render_template('chat.html')
 
 @app.route("/chat/add_message", methods = ["GET", "POST"])
 def add_message():
     message = request.json['messageInputText']
-    print(message)
-    data = {'data': message}
+    user = session.get('username')
+    data = {'data': message, 'user': user}
     return jsonify(data)
 
 
